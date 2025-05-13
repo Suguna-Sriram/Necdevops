8968f20e9829679f7457565ad4b249150014b312
Rajkumarkathiresann <112923221+Rajkumarkathiresann@users.noreply.github.com>
2025-05-13 11:54:12 +0530

    Update add.c
    commit

--- add.c
+++ add.c
@@ -5,7 +5,7 @@ int add(int a, int b) {
 }
 
 int main() {
-    int result = add(4, 4);
-    printf("Sum of 3 and 4 is: %d\n", result);
+    int result = add(5, 4);
+    printf("Sum of 5 and 4 is: %d\n", result);
     return 0;
 }
