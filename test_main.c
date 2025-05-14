46f489f0fb8577b2325737bddeeddd9122148197
Rajkumarkathiresann <112923221+Rajkumarkathiresann@users.noreply.github.com>
2025-05-14 13:11:09 +0530

    Update test_main.c
    update1

--- test_main.c
+++ test_main.c
@@ -3,13 +3,13 @@
 
 int add(int a,int b);
 
-void test_add() {
+void test() {
     assert(add(2, 3) == 5);
     assert(add(-1, 1) == 0);
 }
 
 int main() {
-    test_add();
+    test();
     printf("All tests passed!\n");
     return 0;
 }
